a0f62275342a797c10579b3545233ca52f11d270
Juan David <daviddnet@gmail.com>
2016-06-17 07:55:08 -0500

    se empieza a darle forma al juego

--- resources/game.js
+++ resources/game.js
@@ -1,8 +1,82 @@
-var MemoryGame = function () {    
+"use strict";
 
-    init = function () {
-        console.log("Hello World");
-    };    
+var MemoryGame = function () {
+    var _gameStarted = false;    
+    var _timer = null;
+    var _timerValue = 0;
+    var _flags = [];
+
+    var init = function () {
+        $("#actionButton").on("click", function() {
+            console.log("hi");
+            if (_gameStarted) {
+                stop(true);
+            } else {
+                start();
+            }            
+        });
+
+        $(".card").on("click", function() {
+            if (_gameStarted) {
+                var cardId =  parseInt($(this).attr('id').replace('card',''));
+                
+                console.log(_flags[cardId]);
+            }            
+        });
+    };
+
+    var getDefaultFlags = function() {
+        return [
+            "ar", "bo", "br", "cl","co","cr","ec","ht","jm","mx","pa","pe","py","us","uy","ve"
+        ]
+    };
+
+    var setFlags = function(params) {
+        var random = function() { return (Math.round(Math.random()) - 0.5); }
+        var flags1 = getDefaultFlags().sort(random);
+        var flags2 = getDefaultFlags().sort(random);
+        _flags = flags1.concat(flags2);
+
+        console.log(_flags);
+    };
+
+    var tickTimer = function() {
+        // tick
+        var min = Math.floor(_timerValue / 60);
+        var sec = _timerValue % 60;
+        var output = min + ':' + (sec < 10 ? '0' : '') + sec;
+
+        $("#gameTimer").val(output);
+        _timerValue++;
+    };
+
+    var start = function() {
+        _gameStarted = true;
+        _timer = setInterval(tickTimer, 1000);
+    
+        // get flags in a random order
+        setFlags();
+
+        $("#actionButton")
+            .html("Stop")
+            .removeClass("btn-success")
+            .addClass("btn-primary");
+    };
+
+    var stop = function(clearTimer) {
+        _gameStarted = false;
+        clearInterval(_timer);
+
+        if (clearTimer){
+            _timerValue = 0;
+            $("#gameTimer").val("");
+        }
+
+        $("#actionButton")
+            .html("Start")
+            .removeClass("btn-primary")
+            .addClass("btn-success");
+    };  
 
     return {
         init: init    
